f034521adb6fcde26f2175d5560a7e3104120a11
Thomas Parks <thomasparks@outlook.com>
2019-02-09 18:13:02 +0000

    add gift adding tool.

---
+++ giftadmin.py
@@ -0,0 +1,34 @@
+# /usr/bin/env python3
+# list gifts, add gists, remove gifts.
+from simpledb import Client
+client = Client() # get db access.
+
+import argparse
+parser = argparse.ArgumentParser(description='Manage the giftlist.')
+parser.add_argument("--list", "-l", action='store_true',
+                    help='List gifts currently in the database.')
+
+parser.add_argument("--add", "-a", nargs=2,
+                    help='Adds a gift. --add NAME LINK.')
+
+parser.add_argument("--delete", help='Removes a gift. --delete NAME.', default=None)
+
+args = parser.parse_args()
+
+if args.list:
+    giftnames = client.get("gifts")
+    print(f"List of gifts:\n{giftnames}")
+    
+    if giftnames:
+        for gift in [client.get(name) for name in giftnames]:
+            print(f"{gift['name']}: link - {gift['link']}, claimed - {gift['claimed']} on the {gift['claimdate']}")
+
+if args.add:
+    name, link = args.add[0], args.add[1]
+    newgifts = client.get("gifts")
+    newgifts = newgifts+[name] if newgifts else [name]
+    client.set("gifts", newgifts)
+    client.set(name, {"name":name, "link":link, "claimed":False, "claimdate":None})
+
+if args.delete:
+    print("not impl!!")
