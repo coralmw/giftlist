2e0ea2ec2ef0af5d434a32a0b768181d65a77381
Thomas Parks <thomasparks@outlook.com>
2019-02-09 17:28:21 +0000

    Better styling.

--- giftlist.py
+++ giftlist.py
@@ -2,58 +2,51 @@ from flask import Flask, abort, redirect, url_for
 app = Flask(__name__)
 
 from Cheetah.Template import Template
+import pickle
+from simpledb import Client
+from datetime import date
 
-# need some kind of DB.
-# have a list of gifts, links to buy, and claimed status.
-# captcha?
-# keep backups so can revert list 10min/1h/1day of changes.
+client = Client() # get db access.
 
-class Gift(object):
+# data model: keys are giftnames.
+# key gifts has a list of known gifts.
+# gifts have links, claimed status, and date claimed.
+# need to schedule dumps.
 
-	def __init__(self, name, link, claimed=False):
-		self.name = name
-		self.link = link
-		self.claimed = claimed
-		self.claimdate = None
-
-class GiftDB(object):
-
-	def __init__(self):
-		self.gifts = []
-
-	def __getitem__(self, key):
-		for gift in self.gifts:
-			if gift.name == key:
-				return gift
-		raise KeyError
-
-	def add(self, gift):
-		self.gifts.append(gift)
-
-# list as the prnt order should be consistent.
-DB = GiftDB()
-DB.add(Gift("spoons", "http://the.best.spoons.com"))
-DB.add(Gift("fast car", "http://bmw.co.uk"))
-
-
+# get page templates and static data.
 with open("list.templ", "r") as f:
 	listtempl = f.read()
 
-print(listtempl)
 
 @app.route("/")
 def list():
-	nameSpace = {"giftdb":DB.gifts}
+	gifts = [client.get(name) for name in client.get("gifts")]
+	nameSpace = {"giftdb":gifts, "url_for":url_for}
 	body = Template(listtempl, searchList=[nameSpace])
 	return str(body)
 
 @app.route("/claim/<giftname>", methods=["POST"])
 def claim(giftname):
 	try:
-		gift = DB[giftname]
+		gift = client.get(giftname)
 	except KeyError:
 		abort(400) # gift does not exist.
 
 	# should check if claimed allready and error??
-	DB[giftname].claimed = True
+	gift["claimed"] = True
+	gift["claimdate"] = str(date.today())
+	client.set(giftname, gift)
+	return redirect(url_for("list")) 
+
+@app.route("/release/<giftname>", methods=["POST"])
+def release(giftname):
+	try:
+		gift = client.get(giftname)
+	except KeyError:
+		abort(400) # gift does not exist.
+
+	# should check if claimed allready and error??
+	gift["claimed"] = False
+	gift["claimdate"] = None
+	client.set(giftname, gift)
 	return redirect(url_for("list")) 
